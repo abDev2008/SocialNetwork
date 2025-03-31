dcd0f50322f6124c0e18491b9c3a83c4c317515e
Abel h <abelhailu320@gmail.com>
2025-03-30 23:49:03 -0500

    security with Jwt and configuration implemented

--- src/main/java/com/abletocode/bookstore/user/User.java
+++ src/main/java/com/abletocode/bookstore/user/User.java
@@ -1,11 +1,13 @@
 package com.abletocode.bookstore.user;
 
+import com.abletocode.bookstore.role.Role;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.security.Principal;
@@ -13,6 +15,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
@@ -35,9 +38,8 @@ public class User implements UserDetails, Principal {
     private boolean accountLoceked;
     private boolean enabled;
 
-
-
-
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Role> roles;
 
     @CreatedDate
     @Column(nullable = false, updatable = false)
@@ -48,7 +50,10 @@ public class User implements UserDetails, Principal {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return this.roles
+                .stream()
+                .map(r->new SimpleGrantedAuthority(r.getName()))
+                .collect(Collectors.toList());
     }
 
     @Override
