64c15ea53dceee031d0da8a6935f44e0902aeed9
Abel h <abelhailu320@gmail.com>
2025-04-03 00:09:52 -0500

    added email notification

--- src/main/java/com/abletocode/bookstore/auth/AuthenticationService.java
+++ src/main/java/com/abletocode/bookstore/auth/AuthenticationService.java
@@ -1,6 +1,9 @@
 package com.abletocode.bookstore.auth;
 
+import com.abletocode.bookstore.email.EmailService;
 import com.abletocode.bookstore.role.RoleRepository;
+import com.abletocode.bookstore.user.Token;
+import com.abletocode.bookstore.user.TokenRepository;
 import com.abletocode.bookstore.user.User;
 import com.abletocode.bookstore.user.UserRepository;
 import jakarta.validation.Valid;
@@ -14,6 +17,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -23,7 +28,9 @@ public class AuthenticationService {
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
 
+    private final EmailService emailService;
 
     public void register(RegistrationRequest request) {
     var userRole = roleRepository.findByName("USER")
@@ -38,5 +45,35 @@ public class AuthenticationService {
             .roles(List.of(userRole))
             .build();
 userRepository.save(user);
+sendValidationEmail(user);
+    }
+
+    private void sendValidationEmail(User user) {
+        var newToken = generateAndSaveActivationToken(user);
+
+    }
+
+    private String generateAndSaveActivationToken(User user) {
+        String generatedToken = generateActivationCode(6);
+        var token = Token.builder()
+                .token(generatedToken)
+                .createdAt(LocalDateTime.now())
+                .expiresAt(LocalDateTime.now().plusMinutes(15))
+                .user(user)
+                .build();
+        tokenRepository.save(token);
+        return generatedToken;
+
+    }
+
+    private String generateActivationCode(int length) {
+    String characters = "0123456789";
+    StringBuilder codeBuilder = new StringBuilder();
+    SecureRandom secureRandom = new SecureRandom();
+        for (int i = 0; i < length; i++) {
+            int randomIndex = secureRandom.nextInt(characters.length());
+            codeBuilder.append(characters.charAt(randomIndex));
+        }
+    return codeBuilder.toString();
     }
 }
---
+++ src/main/java/com/abletocode/bookstore/email/EmailTemplateName.java
@@ -0,0 +1,14 @@
+package com.abletocode.bookstore.email;
+
+import lombok.Getter;
+
+@Getter
+public enum EmailTemplateName {
+    ACTIVATE_ACCOUNT("activate account");
+
+    private final String name;
+
+    EmailTemplateName(String name) {
+        this.name = name;
+    }
+}
